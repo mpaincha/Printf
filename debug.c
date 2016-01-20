0ed10de0beb07937a65a4f0cddac0b1edbec1be9
Maelle PAINCHAULT <mpaincha@e2r4p12.42.fr>
2016-01-20 18:54:52 +0100

    creation fichier debug

---
+++ debug.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpaincha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/20 18:46:57 by mpaincha          #+#    #+#             */
+/*   Updated: 2016/01/20 18:47:00 by mpaincha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putlst(t_dbllist	*list)
+{
+	t_elem	*tmp;
+
+	tmp = list->head;
+	ft_putstr("\nAFFICHAGE LISTE: \n");
+	while (tmp != NULL)
+	{
+		ft_putstr("\nMAILLON : \n");
+		ft_putstr("flags : ");
+		ft_putstr(((t_arg *)(tmp->content))->flags);
+		ft_putstr("\nlength : ");
+		ft_putstr(((t_arg *)(tmp->content))->length);
+		ft_putstr("\nprec : ");
+		ft_putstr(((t_arg *)(tmp->content))->prec);
+		ft_putstr("\nspec : ");
+		ft_putstr(((t_arg *)(tmp->content))->spec);
+		ft_putstr("\ntype : ");
+		ft_putstr(((t_arg *)(tmp->content))->type);
+		tmp = tmp->next;
+	}
+}
